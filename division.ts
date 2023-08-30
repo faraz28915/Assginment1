026f99aa7e3cf676bb1d24fec3393d3fa36e276e
faraz28915 <143492672+faraz28915@users.noreply.github.com>
2023-08-30 07:27:52 +0500

    Update division.ts

--- division.ts
+++ division.ts
@@ -2,30 +2,4 @@ export function div(opp: string, num1: number, num2:number){
     let division = num1 / num2;
     console.log("The Division of the two numbers is: " + division)
     return division
-}
-
-
-
-
-
-
-
-
-
-
-/*
-let IsDone = false
-
-let ask: string;
-
-ask = prompt("Are You Done ? ")
-
-ask = ask.toUpperCase
-
-if(ask == "YES"){
-    IsDone = false
-}
-else if (ask == "NO"){
-    IsDone = true
-}
-*/
+}
